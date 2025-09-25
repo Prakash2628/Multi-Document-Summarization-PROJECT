24fd2067708b0695ee6c0d78e2d8b5ee3104a481
Prakash2628 <157944989+Prakash2628@users.noreply.github.com>
2025-09-25 12:42:37 +0530

    Add files via upload

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import DocumentSummarizer from './components/DocumentSummarizer';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-blue-50 via-white to-emerald-50">
+      <DocumentSummarizer />
+    </div>
+  );
+}
+
+export default App;
